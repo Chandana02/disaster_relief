f1da13273969713921e96fb0d65a71ffa23b04dc
Chandana02 <sshchandana.bitra@gmail.com>
2016-02-29 22:09:53 +0530

    created migrations for volunteerInfo and areasInfo
    'volunteers' table contains details of all the volunteers.
    'areas' table contaions between areaName, areaId, cityId.

---
+++ database/migrations/2016_02_29_154059_create_volunteers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVolunteersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('volunteers', function (Blueprint $table) {
+            $table->integer('id')->unique()->primary();
+            $table->string('name', 50);
+            $table->integer('cityId');
+            $table->integer('stateId');
+            $table->bigInteger('contactNo')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('volunteers');
+    }
+}
