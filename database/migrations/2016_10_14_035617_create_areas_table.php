e00c7493cb9d3ba1feb3a030778f8125438a4c33
Chandana02 <sshchandana.bitra@gmail.com>
2016-10-14 23:41:30 +0530

    volunteers migration created

--- database/migrations/2016_10_14_035617_create_areas_table.php
+++ database/migrations/2016_10_14_035617_create_areas_table.php
@@ -14,9 +14,9 @@ class CreateAreasTable extends Migration
     {
         Schema::create('areas', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('state', 255);
-            $table->string('city', 255);
-            $table->string('area', 255);
+            $table->string('state');
+            $table->string('city');
+            $table->string('area');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2016_10_14_232403_create_volunteers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVolunteersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('volunteers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('password');
+            $table->integer('areaCode')->unsigned();
+            $table->string('contact');
+            $table->timestamps();
+        });
+
+        Schema::table('volunteers', function (Blueprint $table) {
+            $table->foreign('areaCode')
+                  ->references('id')
+                  ->on('areas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('volunteers', function (Blueprint $table) {
+            //
+        });
+    }
+}
