9e829a0fabc0065ca67ada64970aea542865b7c1
nk-shruti <nks.shruti@gmail.com>
2016-10-15 11:18:08 +0530

    Applicants Table added
    table for applicants form added

---
+++ database/migrations/2016_10_15_110258_create_applicants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applicants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('state');
+            $table->string('city');
+            $table->string('age');
+            $table->string('gender');
+            $table->string('occupation');
+            $table->string('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::drop('applicants');
+    }
+}
